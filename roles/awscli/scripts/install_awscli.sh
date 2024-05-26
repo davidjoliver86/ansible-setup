03400d9235766cd68c06eba3e0bc41236a1f9396
davidjoliver86 <davidjoliver.2021@gmail.com>
2024-05-26 10:15:53 -0700

    initial commit

---
+++ roles/awscli/scripts/install_awscli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+~/.asdf/bin/asdf plugin add awscli
+~/.asdf/bin/asdf install awscli latest
+~/.asdf/bin/asdf global awscli latest
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -f hosts ]; then
+  cat << EOF > hosts
+[local]
+localhost ansible_connection=local ansible_python_interpreter=/usr/lib/virtualenvs/ansible/bin/python
+
+[local:vars]
+username=$USER
+EOF
+fi
+/usr/lib/virtualenvs/ansible/bin/ansible-playbook --ask-become-pass -i hosts $@
